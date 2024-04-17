665e899054477da1b0c643eddaee4d6dfee657ad
BOARAVORAYOON <yoonbora1803@naver.com>
2024-04-17 22:43:23 +0900

    버튼 클릭 이벤트 스크립트 완성

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 
 //resize
-const menuRW = document.querySelector(".menu-pc");
-const menuList = document.querySelector(".menu-list");
-const tabMenu = document.querySelector(".content-story > div")
-const storyContent = document.querySelector(".box-story")
-const presentContents = document.querySelectorAll(".present")
+const menuRW = document.querySelector(".menu-mobile");
+const storyContent = document.querySelector(".box-story");
 
 window.onresize = function singularResize() {
 	const windowWidth = window.innerWidth;
 
 	if(windowWidth >= 1024){
 		menuRW.className = "menu-pc";
-		menuList.className += " flex";
-		tabMenu.className += " flex";
-		storyContent.className += " grid"
-		for (let i = 0; i < presentContents.length; i++) {
-			presentContents[i].className += " flex";
-		}
-		
+		storyContent.className += " grid"		
 	} else {
 		menuRW.className = "menu-mobile";
-		menuList. className = "menu-list";
-		tabMenu.className = "";
 		storyContent.className = "";
-		for (let i = 0; i < presentContents.length; i++) {
-			presentContents[i].classList.remove("flex");			
-		}
 	}
 }
 
 //mobilemenu button
+const menuButton = document.querySelector(".button-menu");
+const menuDisplay = document.querySelector(".menu-display");
+
+menuButton.addEventListener("click", function () {
+	if (menuDisplay.style.display === "none") {
+		menuDisplay.style.display = "block";
+	} else {
+		menuDisplay.style.display = "none";
+	}
+});
+
+//toggle accordion
+const toggleButton = document.querySelectorAll(".toggle-button");
+const toggleDescription = document.querySelectorAll(".toggle-detail")
+const toggleIcon = document.querySelectorAll(".expand-more")
+
+console.log(toggleIcon[1]);
+
+for (let i = 0; i < toggleButton.length; i++) {
+	toggleButton[i].addEventListener ("click", function () {
+		toggleDescription[i].classList.toggle("toggle-display");
+		toggleIcon[i].classList.toggle("expanded")
+	})
+}
